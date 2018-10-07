5faa473b868015aa99a06b0f923d2a720077fa4b
Ben Lackey <ben.lackey@outlook.com>
2018-10-07 14:31:20 -0700

    everything is better with cat

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/sh
 
 # consolidate into a single markdown document
-markdown-pp book.mdpp -o book.md
+cat "../chapters/00 - Introduction.md" > book.md
+cat "../chapters/01 - 5203 Pershing.md" >> book.md
+cat "../chapters/02 - 1420 Terry.md" >> book.md
+cat "../chapters/03 - 2324 1st.md" >> book.md
+cat "../chapters/04 - 1122 24th.md" >> book.md
+cat "../chapters/05 - 616 N Oakes.md" >> book.md
+cat "../chapters/06 - 1414 N Oakes.md" >> book.md
+cat "../chapters/07 - 3019 63rd.md" >> book.md
+cat "../chapters/08 - The Summing Up.md" >> book.md
+cat "../chapters/09 - Selling.md" >> book.md
+cat "../chapters/10 - Flipping.md" >> book.md
+cat "../chapters/11 - How much house can I afford?.md" >> book.md
+cat "../chapters/12 - REITs.md" >> book.md
+cat "../chapters/13 - The Low End.md" >> book.md
+cat "../chapters/Appendix - Recommended Vendors.md" >> book.md
+cat "../chapters/Appendix - Suggested Reading.md" >> book.md
 
 # convert that into an epub document
 pandoc \
--- build/install.sh
+++ build/install.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/sh
 
-### apparently I don't have pip
-brew install python@2
-
-### markdown-pp
-cd ~
-git clone https://github.com/jreese/markdown-pp.git
-cd markdown-pp
-pip install MarkdownPP
-
 ### pandoc
 sudo chown -R $(whoami) /usr/local/bin /usr/local/lib
 brew install pandoc
