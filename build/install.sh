13ae47eededd864d0bf883b1395f8720c2f58869
Ben Lackey <ben.lackey@outlook.com>
2018-10-07 13:07:34 -0700

    more install fun

--- build/install.sh
+++ build/install.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/sh
 
+### apparently I don't have pip
+brew install python@2
+
+### markdown-pp
+cd ~
+curl -O https://codeload.github.com/jreese/markdown-pp/tar.gz/v1.4
+mv v1.4 markdown-pp-1.4.tar.gz
+tar -xvf markdown-pp-1.4.tar.gz
+cd markdown-pp-1.4
+pip install MarkdownPP
+
 ### pandoc
 sudo chown -R $(whoami) /usr/local/bin /usr/local/lib
 brew install pandoc
-
-### markdown-pp
-curl -O https://github.com/jreese/markdown-pp/archive/v1.4.tar.gz
-pip install MarkdownPP
