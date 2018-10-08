32b6c0b623d3f6077c7716776a5973f28ca9ced5
Ben Lackey <ben.lackey@outlook.com>
2018-10-07 19:34:05 -0700

    newlines

--- build/build.sh
+++ build/build.sh
@@ -2,20 +2,35 @@
 
 # consolidate into a single markdown document
 cat "../chapters/00 - Introduction.md" > book.md
+echo "\n" >> book.md
 cat "../chapters/01 - 5203 Pershing.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/02 - 1420 Terry.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/03 - 2324 1st.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/04 - 1122 24th.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/05 - 616 N Oakes.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/06 - 1414 N Oakes.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/07 - 3019 63rd.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/08 - The Summing Up.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/09 - Selling.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/10 - Flipping.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/11 - How much house can I afford?.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/12 - REITs.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/13 - The Low End.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/Appendix - Recommended Vendors.md" >> book.md
+echo "\n" >> book.md
 cat "../chapters/Appendix - Suggested Reading.md" >> book.md
 
 # convert book.md into an epub document
